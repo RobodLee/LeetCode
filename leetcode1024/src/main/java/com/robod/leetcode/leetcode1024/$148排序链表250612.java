3da8c7f05e2d4a7a0a43580fd9762bfd27b12cdd
Robod <robodlee@gmail.com>
2025-06-14 19:42:03 +0800

    $108将有序数组转换为二叉搜索树250614

---
+++ leetcode1024/src/main/java/com/robod/leetcode/leetcode1024/$148排序链表250612.java
@@ -0,0 +1,14 @@
+package com.robod.leetcode.leetcode1024;
+
+import com.robod.leetcode.common.entity.ListNode;
+
+/**
+ * https://leetcode.cn/problems/sort-list
+ */
+public class $148排序链表250612 {
+
+    public ListNode sortList(ListNode head) {
+
+    }
+
+}
